91dd9863a9d9e8bece0d2596959197c7583c512e
Maike Rodrigues <maikedev7@gmail.com>
2021-09-12 12:29:55 -0300

    first commit

---
+++ src/test/java/com/maike/rodrigues/citiesapi/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maike.rodrigues.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
