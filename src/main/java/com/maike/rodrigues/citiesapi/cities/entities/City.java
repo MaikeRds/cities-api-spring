3182f9c4d2583aedc24735dbc21c3526fb8637c6
Maike Rodrigues <maikedev7@gmail.com>
2021-09-12 15:03:17 -0300

    Created City

---
+++ src/main/java/com/maike/rodrigues/citiesapi/cities/entities/City.java
@@ -0,0 +1,45 @@
+package com.maike.rodrigues.citiesapi.cities.entities;
+
+import lombok.*;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+import org.springframework.data.geo.Point;
+
+import javax.persistence.*;
+
+
+@Entity(name = "City")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "cidade")
+@TypeDefs(value = {
+        @TypeDef(name = "point", typeClass = PointType.class)
+})
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "nome")
+    private String name;
+
+    private Integer uf;
+
+    private Integer ibge;
+
+    // 1st
+    @Column(name = "lat_lon")
+    private String geolocation;
+
+    // 2nd
+    @Type(type = "point")
+    @Column(name = "lat_lon", updatable = false, insertable = false)
+    private Point location;
+
+}
