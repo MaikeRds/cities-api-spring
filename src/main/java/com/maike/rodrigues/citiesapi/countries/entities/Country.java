df50b6b207e887c1aeb0fc4ed7f92ec70420fcbc
Maike Rodrigues <maikedev7@gmail.com>
2021-09-12 13:33:23 -0300

    Created entity countries

---
+++ src/main/java/com/maike/rodrigues/citiesapi/countries/entities/Country.java
@@ -0,0 +1,41 @@
+package com.maike.rodrigues.citiesapi.countries.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name = "Country")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "pais")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "nome")
+    private String name;
+
+    @Column(name = "nome_pt")
+    private String portugueseName;
+
+    @Column(name = "sigla")
+    private String code;
+
+    private Integer bacen;
+
+}
